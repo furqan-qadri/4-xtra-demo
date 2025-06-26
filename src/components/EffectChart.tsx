579948de5b16923c1c52e73fc2c88a1b4ea26956
“Furqan <furqaanqadri@gmail.com>
2025-06-26 12:22:36 +0100

    working with image just need to add stuff around it

--- src/components/EffectChart.tsx
+++ src/components/EffectChart.tsx
@@ -19,21 +19,28 @@ const EffectChart: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col relative items-center p-12 gap-4 w-full h-full bg-red-500 rounded-lg overflow-hidden">
+    <div 
+      className="flex flex-col relative items-center p-12 gap-4 w-full h-full rounded-lg overflow-hidden bg-contain bg-center bg-no-repeat"
+      style={{
+        backgroundImage: 'url(/assets/sandp_new.png)'
+      }}
+    >
       {/* Red square container */}
       {/* <div className="relative w-64 h-64 bg-red-500 rounded-lg overflow-hidden"> */}
         {/* Yellow div positioned 15% from bottom and 20% from left */}
         <div 
-          className={`absolute h-3/5 bg-yellow-400 ${
+          className={`absolute h-3/5 bg-white ${
             isDisappearing 
               ? 'transform translate-x-full opacity-100' 
               : 'transform translate-x-0 opacity-100'
           }`}
           style={{
-            bottom: '15%',
-            left: '20%',
+            bottom: '25%',
+            left: '40%',
             right: '0%',
-            width: '80%',
+            top: '25%',
+            width: '90%',
+            height: '50%',
             transition: 'all 6s ease-in-out'
           }}
           onTransitionEnd={handleAnimationEnd}
